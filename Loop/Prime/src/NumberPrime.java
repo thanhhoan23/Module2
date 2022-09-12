f27cddf31adae7b4c819a62faaa7cdfe2d24156d
Nguyễn Thị Thanh Hoan <nguyenthithanhhoan@Thanh-hoan-2.local>
2022-09-12 16:43:19 +0700

    BaiTap

---
+++ Loop/Prime/src/NumberPrime.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class NumberPrime {
+    public static void main(String[] args) {
+      int count = 0;
+//      xem so luong so nguyen to in ra da bang number chua
+      int number = 0;
+
+      for (int n = 2; n <20; n++ ){
+          if (count == number) {
+
+          }
+      }
+    }
+}
