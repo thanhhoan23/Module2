4874fd956490effc866ae416bfd73becf4d3a3aa
Nguyễn Thị Thanh Hoan <nguyenthithanhhoan@Thanh-hoan-2.local>
2022-09-13 17:13:23 +0700

    BaiTap

--- Array/FindMax/src/FindMaxValue.java
+++ Array/FindMax/src/FindMaxValue.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class FindMaxValue {
+import java.util.Scanner;
+
+public class FindMaxValue {
+    public static void main(String[] args) {
+//        String [] names = {
+//                "Quockkkhanh", "DuykHANH", "tHANHHOAN"
+//        };
+//        String str = names[0];
+//        for (int i = 0; i < names.length; i++){
+//            if (names[i].trim().length() > str.trim().length()) {
+//                str = names[i];
+//            }
+//        }
+//        System.out.println(str);
+
+//        Tim so lon nhat
+
+        Integer [] numbers = {
+               11,1,5,2,9,10
+        };
+        int num = numbers[0];
+        for (int i = 0; i< numbers.length; i++) {
+            if (numbers[i] > num) {
+                num = numbers[i];
+            }
+        }
+        System.out.println(num);
+
+    }
 }
